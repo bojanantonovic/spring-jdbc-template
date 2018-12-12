93945c1543ef82a73ce0d1838406dd962c0fa98a
Bojan Antonovic <bojan@antonovic.ch>
2018-12-13 00:02:37 +0100

    var / type interference

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,18 +11,18 @@ public class Main {
     static final String SELECT_FROM_PERSON = "select * from person";
 
     public static void main(String... args) {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        var dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(DRIVER_CLASS_NAME);
         dataSource.setUrl("jdbc:sqlserver://localhost");
         dataSource.setUsername("SA");
         dataSource.setPassword("Bojan123");
 
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        var jdbcTemplate = new JdbcTemplate(dataSource);
 
         jdbcTemplate.update(INSERT_INTO_PERSON, "Donald", "Duck");
         jdbcTemplate.update(INSERT_INTO_PERSON, "Mickey", "Mouse");
 
-        final List<Map<String, Object>> list = jdbcTemplate.queryForList(SELECT_FROM_PERSON);
+        var list = jdbcTemplate.queryForList(SELECT_FROM_PERSON);
         System.out.println(list);
     }
 
